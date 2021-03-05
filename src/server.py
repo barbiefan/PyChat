60484d13dde1203720e4faf8c960d36b60ce6fda
obey <georgecarlin770@gmail.com>
2021-03-05 03:17:35 +0300

    made server a proper class

--- src/server.py
+++ src/server.py
@@ -2,33 +2,57 @@ import socket
 import os
 from _thread import start_new_thread
 
-ServerSocket = socket.socket()
-host = '127.0.0.1'
-port = 5005
-ThreadCount = 0
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+class Server():
 
-print('Waitiing for a Connection..')
-ServerSocket.listen(5)
+    def __init__(self, IP='127.0.0.1', PORT=5005, backlog=10):
+        self.IP = IP
+        self.PORT = PORT
+        self.backlog = backlog
+        self.Socket = socket.socket()
+        try:
+            self.Socket.bind((self.IP, self.PORT))
+        except socket.error as err:
+            print('INITIALIZATION ERROR:')
+            print(str(err))
+        else:
+            print(f'Server initialized. IP:PORT - {self.IP}:{self.PORT}')
+        self.msg_log = []
 
 
-def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Servern'))
-    while True:
-        data = connection.recv(2048)
-        reply = 'Server Says: ' + data.decode('utf-8')
-        if not data:
-            break
-        connection.sendall(str.encode(reply))
-    connection.close()
+    def listen(self, backlog=None):
+        if backlog:
+            self.backlog = backlog
+        try:
+            self.Socket.listen(self.backlog)
+            print(f'Listening on {self.IP}:{self.PORT}\nbacklog: {self.backlog}')
+            while True:
+                Client, address = self.Socket.accept()
+                print(f'Connected to: {address[0]}:{address[1]}')
+                start_new_thread(self.client_thread, (Client, ))
+        except Exception as err:
+            print('SERVER ERROR:')
+            print(str(err))
+            print('closing Socket...')
+            self.Socket.close()
+            print('done.')
 
-while True:
-    Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSocket.close()
+    def client_thread(self, connection):
+        client_address = connection.getpeername()
+        try:
+            while True:
+                data = connection.recv(2048).decode('utf-8')
+                self.msg_log.append(data)
+                reply = f'server\'s log:\n{self.msg_log}'
+                if not data:
+                    break
+                connection.sendall(str.encode(reply))
+        except Exception as err:
+            print(f'CLIENT CONNECTION ERROR ({client_address[0]}:{client_address[1]}):')
+            print(str(err))
+        finally:
+            print(f'closing connection {client_address[0]}:{client_address[1]} ...')
+            connection.close()
+            print('done.')
+
+serv = Server()
+serv.listen()
