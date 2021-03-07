dabbbd68c9c34edaa41a0dfac9b3edcefb5519b4
hatsukoi13 <beije9462@gmail.com>
2021-03-07 23:31:38 +0300

    Test Packet class

--- src/backbone/Packet.py
+++ src/backbone/Packet.py
@@ -1,14 +1,20 @@
 import datetime
 import json
+import time
 
 test_string = '{"Sender": "Xxx_Pussy_destroyer_xxX", "DateTimeReceived": "08.03.2021 18:02:33", "PacketCode": "MS", "Content": "test message for parsing"}'
-
+json = json.loads(test_string)
 
 
 class Packet():
     def __init__(self, string: str):
-        pass
+        self.sender = json["Sender"]
+        if json["DateTimeReceived"] == "":
+            self.datetime = str(time.time())
+        else:
+            self.datetime = json["DateTimeReceived"]
+        self.packetcode = json["PacketCode"]
+        self.content = json["Content"]
+        
 
-
-
-packet = Packet(test_string)
+packet = Packet(json)
