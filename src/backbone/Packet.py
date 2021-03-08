b3f9f6ab9e0c6244badedcab24fd17e9f50a4783
hatsukoi13 <beije9462@gmail.com>
2021-03-08 20:55:29 +0300

    ---

--- src/backbone/Packet.py
+++ src/backbone/Packet.py
@@ -5,6 +5,7 @@ import time
 test_string = '{"Sender": "Xxx_Pussy_destroyer_xxX", "DateTimeReceived": "08.03.2021 18:02:33", "PacketCode": "MS", "Content": "fuck u bitch"}'
 
 class Packet():
+    
     def __init__(self, string: str):
         slovar = json.loads(string)
         self.Sender = slovar["Sender"]
@@ -14,6 +15,7 @@ class Packet():
             self.DateTime = slovar["DateTimeReceived"]
         self.PacketCode = slovar["PacketCode"]
         self.Content = slovar["Content"]
+    
     def to_json(self):
         attributes = {
             'Sender': self.Sender,
@@ -21,9 +23,7 @@ class Packet():
             'PacketCode': self.PacketCode,
             'Content': self.Content 
             }
-        result = json.dumps(attributes)
-        print (result)
+        return json.dumps(attributes)
         
 
 packet = Packet(test_string)
-packet.to_json()
